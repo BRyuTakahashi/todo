1e7329e2f37802558249a1940b1eef3adbb537f0
Bruno Ryu Takahashi <brunoryu0805@gmail.com>
2025-10-27 21:54:49 -0300

    Task Sumary Component done

---
+++ src/pages/PageHome.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../components/Container";
+import { TasksSumary } from "../core-components/TasksSumary";
+
+export function PageHome() {
+    return (
+        <Container as="article" className="space-y-3">
+            <header className="flex items-center justify-between">
+                <TasksSumary></TasksSumary>
+            </header>
+        </Container>
+    )
+}
