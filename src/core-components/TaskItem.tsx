041b7de30e3002a62a7f1a52b26177069e21b12f
Bruno Ryu Takahashi <brunoryu0805@gmail.com>
2025-10-28 09:28:52 -0300

    creating and editing item done

--- src/core-components/TaskItem.tsx
+++ src/core-components/TaskItem.tsx
@@ -6,11 +6,20 @@ import TrashIcon from '../assets/icons/trash.svg?react'
 import PencilIcon from '../assets/icons/pencil.svg?react'
 import XIcon from '../assets/icons/x.svg?react'
 import CheckIcon from "../assets/icons/check.svg?react"
-import { useState } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import { InputText } from "../components/InputText";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
+import { useTask } from "../hooks/useTask";
 
-export function TaskItem() {
-    const [isEditing, setIsEditing] = useState(false)
+interface Props {
+    task: Task;
+}
+
+export function TaskItem({ task }: Props) {
+    const [isEditing, setIsEditing] = useState(task?.state === TaskState.Creating)
+    const [taskTitle, setTaskTitle] = useState(task.title || '')
+    const { updateTask } = useTask()
 
     function handleEditTask() {
         setIsEditing(true);
@@ -20,26 +29,58 @@ export function TaskItem() {
         setIsEditing(false)
     }
 
+    function handleChangeTaskTitle(e: ChangeEvent<HTMLInputElement>) {
+        setTaskTitle(e.target.value || '')
+    }
+
+    function handleSaveTask(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        updateTask(task.id, { title: taskTitle })
+        setIsEditing(false)
+    }
+
     return (
-        <Card size='md' className="flex items-center gap-4">
+        <Card size='md' >
             {!isEditing ?
-                <>
-                    <InputCheckbox />
-                    <Text className="flex-1">🛒 Fazer compras da semana</Text>
+                <div className="flex items-center gap-4">
+                    <InputCheckbox
+                        value={task?.concluded?.toString()}
+                        checked={task.concluded}
+                    />
+                    <Text className={cx("flex-1", { 'line-through': task?.concluded }
+                    )}>
+                        {task?.title}
+                    </Text>
                     <div className="flex gap-1">
                         <ButtonIcon icon={TrashIcon} variant='tertiary' />
                         <ButtonIcon icon={PencilIcon} variant='tertiary' onClick={handleEditTask} />
                     </div>
-                </>
+                </div>
                 :
-                <>
-                    <InputText className="flex-1" />
+                <form className="flex items-center gap-4" onSubmit={handleSaveTask}>
+                    <InputText
+                        value={taskTitle}
+                        className="flex-1"
+                        onChange={handleChangeTaskTitle}
+                        required
+                        autoFocus
+                    />
                     <div className="flex gap-1">
-                        <ButtonIcon icon={XIcon} variant='secondary' onClick={handleExitEditTask} />
-                        <ButtonIcon icon={CheckIcon} variant='primary' />
+                        <ButtonIcon
+                            type="button"
+                            icon={XIcon}
+                            variant='secondary'
+                            onClick={handleExitEditTask}
+                        />
+                        <ButtonIcon
+                            type='submit'
+                            icon={CheckIcon}
+                            variant='primary'
+                        />
                     </div>
-                </>
+                </form>
             }
         </Card>
+
     )
 }
--- src/core-components/TasksList.tsx
+++ src/core-components/TasksList.tsx
@@ -1,18 +1,32 @@
 import { Button } from "../components/Button";
 import { TaskItem } from "./TaskItem";
 import PlusIcon from '../assets/icons/plus.svg?react'
+import { useTasks } from "../hooks/useTasks";
+import { useTask } from "../hooks/useTask";
+import { TaskState } from "../models/task";
 
 export function TasksList() {
+    const { tasks } = useTasks();
+    const { prepareTask } = useTask();
+
+    function handleNewTask() {
+        prepareTask()
+    }
+
     return (
         <>
             <section>
-                <Button icon={PlusIcon} className="w-full">Nova tarefa</Button>
+                <Button
+                    icon={PlusIcon}
+                    className="w-full"
+                    onClick={handleNewTask}
+                    disabled={tasks.some((task) => task.state === TaskState.Creating)}
+                >
+                    Nova tarefa
+                </Button>
             </section>
             <section className="space-y-2">
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
+                {tasks.map((task) => <TaskItem key={task.id} task={task} />)}
             </section>
         </>
     )
